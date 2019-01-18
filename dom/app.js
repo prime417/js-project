d7258099a383d4ceb6129cfc1e6c6eb4d67ae3fd
prime417 <pankajds417@gmail.com>
2019-01-18 13:06:08 +0530

    remove and filter element added

--- dom/app.js
+++ dom/app.js
@@ -1,6 +1,6 @@
 //Define UI Vars
-const form =  document.querySelector('#task-form');
-const taskList  = document.querySelector('.collection');
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
@@ -8,12 +8,21 @@ const taskInput = document.querySelector('#task');
 //Load all event listeners
 loadEventListeners();
 
-function loadEventListeners(){
-form.addEventListener('submit',addTask);
+//Load all event listeners
+function loadEventListeners() {
+  //add task event
+  form.addEventListener('submit', addTask);
+  //remove task event
+  taskList.addEventListener('click', removeTask);
+  //clear tasks event
+  clearBtn.addEventListener('click', clearTask);
+  //filter tasks event
+  filter.addEventListener('keyup', filterTask);
 }
 
-function addTask(e){
-  if(taskInput.value === ''){
+// add Task
+function addTask(e) {
+  if (taskInput.value === '') {
     alert('Add a task');
   }
 
@@ -31,12 +40,47 @@ function addTask(e){
   link.innerHTML = '<i class= "fa fa-remove"</i>';
   //Append the link to li 
   li.appendChild(link);
-
   // append li to ul
-   taskList.appendChild(li);
-  
-//clear input
-   taskInput.value ='';
+  taskList.appendChild(li);
+  //clear input
+  taskInput.value = '';
+  //prevent redirect
   e.preventDefault();
 
+}
+
+//Remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//clear Tasks
+function clearTask() {
+  // taskList.innerHTML = "";
+
+  //faster
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+//filter tasks
+function filterTask(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(
+    function (task) {
+      const item = task.firstChild.textContent;
+
+      if (item.toLowerCase().indexOf(text) != -1) {
+        task.style.display = 'block';
+      } else {
+        task.style.display = "none";
+      }
+    }
+  );
 }
