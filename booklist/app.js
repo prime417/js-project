8f737adb9c9adabc1784a0f2045f51e368fd2895
prime417 <pankajds417@gmail.com>
2019-01-22 19:36:17 +0530

    BookList: validation added

--- booklist/app.js
+++ booklist/app.js
@@ -23,6 +23,29 @@ UI.prototype.addBookList = function (book) {
   list.appendChild(row);
 };
 
+// show alert 
+UI.prototype.showAlert = function (message, className) {
+  //create div
+  const div = document.createElement('div');
+
+  // add classes
+  div.className = `alert ${className}`;
+  // add text
+  div.appendChild(document.createTextNode(message));
+  //get parent 
+  const container = document.querySelector('.container');
+
+  //get form
+  const form = document.querySelector('#book-form');
+  // insert alert
+  container.insertBefore(div, form);
+
+  // timeout after 3sec
+  setTimeout(function () {
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
 // clear fields
 UI.prototype.clearFields = function () {
   document.getElementById("title").value = '';
@@ -42,11 +65,19 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
   // Instantiate UI
   const ui = new UI();
 
-  // Add book to list
-  ui.addBookList(book);
+  //Validate
+  if (title === '' || author === '' || isbn === '') {
 
-  //clear fields
-  ui.clearFields();
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    // Add book to list
+    ui.addBookList(book);
+    'success';
+    // show alert 
+    ui.showAlert('Book Added', 'success');
+    //clear fields
+    ui.clearFields();
+  }
 
   e.preventDefault();
 });
